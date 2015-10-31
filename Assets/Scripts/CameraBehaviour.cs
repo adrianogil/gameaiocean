648325a3b03a73ad19b11aaa3cbd7cf6a9a700bf
Adriano Gil <adriano.gil@indt.org.br>
2015-10-31 09:50:01 -0400

    Exemplor do cubo

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehaviour : MonoBehaviour {
+
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+		transform.position = target.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = target.position;
+	}
+}
---
+++ Assets/Scripts/CubeBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeBehaviour : MonoBehaviour {
+
+	public float velocity = 10f;
+
+	float horizontalAxis;
+	Rigidbody cubeRigidBody;
+
+	// Use this for initialization
+	void Start () {
+		cubeRigidBody = GetComponent <Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		horizontalAxis = Input.GetAxis ("Horizontal");
+
+		if (horizontalAxis > 0) {
+			transform.Rotate (0f, 0f, 1f);
+			cubeRigidBody.velocity += velocity * Vector3.right * Time.deltaTime;
+		} else if (horizontalAxis < 0) {
+			transform.Rotate (0f, 0f, -1f);
+			cubeRigidBody.velocity += velocity * Vector3.left * Time.deltaTime;
+		}
+
+	}
+}
