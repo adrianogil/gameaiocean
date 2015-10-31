f7fc62cbaeb79db1f3de12c98a7c38a2801d4866
Adriano Gil <adriano.gil@indt.org.br>
2015-10-31 14:42:26 -0400

    Incredible example of procedural generation

---
+++ Assets/Scripts/PrefabGeneration.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrefabGeneration : MonoBehaviour {
+
+	public Transform prefab;
+
+	// Use this for initialization
+	void Start () {
+
+		Instantiate (prefab, Vector3.zero, Quaternion.identity);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
