afd4dcd878d5aa3fde37749f0141140b6c8824d4
Adriano Gil <adriano.gil@indt.org.br>
2015-10-31 15:50:34 -0400

    Tree Generation

--- Assets/Scripts/PrefabGeneration.cs
+++ Assets/Scripts/PrefabGeneration.cs
@@ -24,11 +24,14 @@ public class PrefabGeneration : MonoBehaviour {
 		prefabSizeZ = prefabRenderer.bounds.size.z;
 
 		FloorGeneration ();
+
+		TreeGeneration ();
 	}
 
 	void FloorGeneration()
 	{
 		Vector3 cubePosition = Vector3.zero;
+		Transform cube;
 
 		for (int y = 0; y < floorSizeY; y++) {
 
@@ -39,9 +42,60 @@ public class PrefabGeneration : MonoBehaviour {
 
 				cubePosition.x += prefabSizeX;
 
-				Instantiate (prefab, cubePosition, Quaternion.identity);
+				cube = Instantiate (prefab, cubePosition, Quaternion.identity) as Transform;
+
+				cube.parent = transform;
 			}
 		}
+	}
+
+	void TreeGeneration()
+	{
+		int treePosX = 0;
+		int treePosY = 0;
+
+		int trunkSize = 5;
+
+		int treeSizeX = 5;
+		int treeSizeY = 5;
+		int treeSizeZ = 5;
+
+		Vector3 treePosition = new Vector3((treePosX+1) * prefabSizeX, 
+		                                   prefabSizeY,
+		                                   (treePosY+1) * prefabSizeZ);
+
+		Transform cube;
+		Vector3 cubePosition = treePosition;
+
+		GameObject tree = new GameObject ("Tree");
+
+		for (int t = 0; t < trunkSize; t++) {
+			cube = Instantiate (prefab, cubePosition, Quaternion.identity) as Transform;
+			cube.transform.parent = tree.transform;
+
+			cubePosition.y += prefabSizeY;
+		}
+
+		cubePosition.x -= (treeSizeX/2) * prefabSizeX;
+		cubePosition.z -= (treeSizeZ/2) * prefabSizeZ;
+
+		treePosition = cubePosition;
+
+
+		for (int z = 0; z < treeSizeZ; z++) {
+			cubePosition.y = treePosition.y;
+			for (int y = 0; y < treeSizeY; y++) {
+				cubePosition.x = treePosition.x;
+				for (int x = 0; x < treeSizeX; x++) {
+					cube = Instantiate (prefab, cubePosition, Quaternion.identity) as Transform;
+					cube.transform.parent = tree.transform;
+					cubePosition.x += prefabSizeX;
+				}
+				cubePosition.y += prefabSizeY;
+			}
+			cubePosition.z += prefabSizeZ;
+		}
+
 	}
 	
 	// Update is called once per frame
