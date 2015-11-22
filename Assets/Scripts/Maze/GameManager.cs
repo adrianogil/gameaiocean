3a2699b66515edc65d17434e21d7207996f15b1b
Adriano Gil <adriano.gil@indt.org.br>
2015-11-22 19:10:51 -0400

    Exemplo do Labirinto

---
+++ Assets/Scripts/Maze/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public Maze mazePrefab;
+
+	private Maze mazeInstance;
+
+	// Use this for initialization
+	void Start () {
+		BeginGame ();
+	}
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			RestartGame();
+		}
+	}
+	
+	private void BeginGame () {
+		mazeInstance = Instantiate (mazePrefab) as Maze;
+		StartCoroutine(mazeInstance.Generate ());
+	}
+
+	private void RestartGame () {
+		StopAllCoroutines ();
+
+		Destroy (mazeInstance.gameObject);
+
+		BeginGame ();
+	}
+}
---
+++ Assets/Scripts/Maze/MazeCell.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MazeCell : MonoBehaviour {
+
+	public IntVector2 coordinates;
+
+	private int initializedEdgeCount;
+
+	public bool IsFullyInitialized {
+		get {
+			return initializedEdgeCount == MazeDirections.Count;
+		}
+	}
+
+	private MazeCellEdge[] edges = new MazeCellEdge[MazeDirections.Count];
+
+	public MazeDirection RandomUninitializedDirection {
+		get {
+			int skips = Random.Range(0, MazeDirections.Count - initializedEdgeCount);
+
+			for (int i = 0; i < MazeDirections.Count; i++)
+			{
+				if (edges[i] == null) {
+					if (skips == 0) {
+						return (MazeDirection) i;
+					}
+					skips -= 1;
+				}
+			}
+			//throw new System.InvalidOperationException("MazeCell no tem mais direcoes naum");
+		
+			return MazeDirection.Invalid;
+		}
+	}
+
+	public MazeCellEdge GetEdge(MazeDirection direction) {
+		return edges[ (int) direction];
+	}
+
+	public void SetEdge(MazeDirection direction, MazeCellEdge edge) {
+		edges[ (int) direction] = edge;
+
+		initializedEdgeCount += 1;
+	}
+
+}
