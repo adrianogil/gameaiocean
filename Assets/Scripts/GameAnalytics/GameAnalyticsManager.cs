f84ffe189c6a44a256b5372f4990932437a813cb
Adriano Gil <adriano.gil@indt.org.br>
2015-11-27 13:53:32 -0400

    Game Analytics

---
+++ Assets/Scripts/GameAnalytics/GameAnalyticsManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using GameAnalyticsSDK;
+
+public class GameAnalyticsManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		PlayerPrefs.SetInt ("loaded_maze", PlayerPrefs.GetInt ("loaded_maze", 0) + 1);
+
+		GameAnalytics.NewDesignEvent ("maze_scene:loading", PlayerPrefs.GetInt ("loaded_maze", 0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
